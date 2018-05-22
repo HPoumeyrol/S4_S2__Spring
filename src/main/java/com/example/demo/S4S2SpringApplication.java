5d6ba6133c79a47b1603dae74d576913f8887f38
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-05-22 10:20:14 +0200

    First Commit

---
+++ src/main/java/com/example/demo/S4S2SpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4S2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4S2SpringApplication.class, args);
+	}
+}
